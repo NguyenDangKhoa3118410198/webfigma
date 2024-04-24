39f20706f8c30cda46341c1a61187054fa435da1
Nguyen Dang Khoa <71954389+NguyenDangKhoa3118410198@users.noreply.github.com>
2024-04-24 16:38:42 +0700

    cre: booking details page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="BookingApp">
+         <div className="booking-wrapper">
+            <div className="booking-container">
+               <div className="booking-information">
+                  <div className="booking-basic-inform">
+                     <p className="booking-customer-name">Customer's name</p>
+                     <div className="booking-customer-contact">
+                        <p>
+                           {"{"}
+                           phone number
+                           {"}"}
+                        </p>
+                        <p>
+                           {"{"}
+                           booking day, booking time
+                           {"}"}
+                        </p>
+                     </div>
+                  </div>
+                  <div className="booking-divider"></div>
+                  <div className="booking-details">
+                     <div className="booking-service-details">
+                        <p className="booking-service-title">Service Details</p>
+                        <>
+                           <div className="content-servive-details">
+                              <div className="service-and-price">
+                                 <p className="services">Service's name</p>
+                                 <div className="price-and-quanlity">
+                                    <p className="quanlity">1x</p>
+                                    <p className="price">$50.00</p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="booking-divider"></div>
+                        </>
+                        <>
+                           <div className="content-servive-details">
+                              <div className="service-and-price">
+                                 <p className="services">Service's name</p>
+                                 <div className="price-and-quanlity">
+                                    <p className="quanlity">1x</p>
+                                    <p className="price">$50.00</p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="booking-divider"></div>
+                        </>
+                        <>
+                           <div className="content-service-details">
+                              <div className="service-and-price">
+                                 <p className="services">Service's name</p>
+                                 <div className="price-and-quanlity">
+                                    <p className="quanlity">1x</p>
+                                    <p className="price">$20.00</p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="booking-divider"></div>
+                        </>
+                        <>
+                           <div className="content-servive-details">
+                              <div className="service-and-price">
+                                 <p className="services">Service's name</p>
+                                 <div className="price-and-quanlity">
+                                    <p className="quanlity">1x</p>
+                                    <p className="price">$20.00</p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="booking-divider"></div>
+                        </>
+                     </div>
+                     <>
+                        <div className="booking-balance">
+                           <p className="sub-total">Subtotal</p>
+                           <p className="price">$140.00</p>
+                        </div>
+                        <div className="booking-divider"></div>
+
+                        <div className="booking-balance">
+                           <p className="sub-total bold-text">Total</p>
+                           <p className="price large-font">$140.00</p>
+                        </div>
+
+                        <div className="booking-balance">
+                           <p className="payment-method">Payment method</p>
+                           <p className="price large-font">CARD</p>
+                        </div>
+                     </>
+                  </div>
+               </div>
+               <button className="booking-button">Button</button>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
